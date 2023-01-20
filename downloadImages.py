298926af4481a27f02109fdcb684f80e7c90d944
Cristian Gonzalez Lopez <cristiangl1995@gmail.com>
2023-01-20 18:13:36 +0100

    clean download image function

--- downloadImages.py
+++ downloadImages.py
@@ -7,12 +7,8 @@ from PIL import Image
 MARVEL_ZONE_CARDS_IMAGE_URL = "https://static.marvelsnap.pro/cards/"
 
 def resize(imagePath):
-        fixed_height = 1024
         image = Image.open(imagePath)
         image = image.crop((125, 0, image.width - 125, image.height))
-        height_percent = (fixed_height / float(image.size[1]))
-        width_size = int( float(image.size[0]) * float(height_percent) )
-        # image = image.resize((width_size, fixed_height), PIL.Image.NEAREST)
         image.save(imagePath, 'webp', optimize=True, quality=10)
 
 def descargar_imagenes(nombres, carpeta):
